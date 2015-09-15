e000e7c57bdd750150cd953f33883f4a288ea7ad
前田 和寛 <kazutan@maeda-mba.local>
2015-09-15 10:55:30 +0900

    v0.10

--- generateIndex.R
+++ generateIndex.R
@@ -6,12 +6,19 @@ local({
 title: "GitとGitHubとRStudio"
 output: html_document
 ---
+
+## このドキュメントについて
+このドキュメントは、「魁!! 広島ベイズ塾 2015夏合宿」のために用意したドキュメントです。
+
+なにかありましたら、[Twitterアカウント](https://twitter.com/kazutan) へメンション、もしくは[このリポジトリ](https://github.com/kazutan/RStudio2Github) のissueまでお願いします。
+
+## Index
 ', file = md_tmp)
 
   rmd_pages  <- list.files(pattern = "*.Rmd")
   html_pages <- sub(".Rmd", ".html", rmd_pages)
   mtimes     <- file.mtime(rmd_pages)
-  page_list  <- sort(sprintf("* %s [%s](%s)", mtimes, html_pages, html_pages))
+  page_list  <- sort(sprintf("* [%s](%s) %s", html_pages, html_pages, mtimes))
   cat(paste0(page_list, collapse = "\n"),
       file = md_tmp, append = TRUE)
 
